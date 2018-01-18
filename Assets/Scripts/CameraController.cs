91239c0af96aa3eb4d33a8bc94ba2c8d8ceccca7
Danny <giantcaib@hotmail.com>
2018-01-18 18:01:14 +1100

    A Completed Version
    Thanks to the YouTuber: Brackeys and his helpful tutorials.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform target;
+
+	public Vector3 offset;
+	public float zoomSpeed = 4f;
+	public float minZoom = 5f;
+	public float maxZoom = 10f;
+
+	public float pitch = 2f;
+	public float yawSpeed = 100f;
+
+	public float currentZoom = 10f;
+	private float currentYaw = 0f;
+
+	void Update(){
+		//zoom in/out
+		currentZoom -= Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
+		currentZoom = Mathf.Clamp (currentZoom,minZoom,maxZoom);
+
+		//move camera with a/d 
+		currentYaw += Input.GetAxis("Horizontal")*yawSpeed*Time.deltaTime;
+	}
+
+	void LateUpdate(){
+		transform.position = target.position - offset * currentZoom;
+		transform.LookAt (target.position + Vector3.up * pitch);
+
+		transform.RotateAround (target.position, Vector3.up, currentYaw);
+	}
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+	public float radius = 3f;
+	public Transform interactionTransform;
+
+	bool isFocus = false;
+	bool hasInteracted = false;
+	Transform player;
+
+	public virtual void Interact(){
+		Debug.Log ("INTERACT");
+	}
+
+	void Update(){
+		if(isFocus&&!hasInteracted){
+			float distance = Vector3.Distance (player.position, interactionTransform.position);
+			//inside the radius
+			if(distance<=radius){
+				Interact ();
+				hasInteracted = true;
+			}
+		}
+	}
+
+	public void OnFocused (Transform playerTransform){
+		isFocus = true;
+		hasInteracted = false;
+		player = playerTransform;
+	}
+
+	public void OnDefocused (){
+		isFocus = false;
+		hasInteracted = false;
+		player = null;
+	}
+
+	void OnDrawGizmosSelected (){
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere (interactionTransform.position, radius);
+
+
+	}
+}
